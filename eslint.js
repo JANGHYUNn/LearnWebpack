c84bcdae75eec06c35e240dcd3d84dcff4854705
JANGHYUNn <kimjh2369@gmail.com>
2020-06-13 04:42:25 +0900

    modify

--- eslint.js
+++ eslint.js
@@ -1,6 +1,6 @@
 //es lint 
 // 포맷팅 : 코드를 일관되게 이쁘게만듬 들여쓰기 
 // 품질: 사전에 문법 오류가 없는지 체크
-// eslint 설치 npx eslint --init .eslintrc.js 기본설정헤줌
+// eslint 설치 npx eslint --init .eslintrc.js 기본설정헤줌.
 console.log();
 (function(){})()
